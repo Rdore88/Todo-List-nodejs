4ee738589a6bff6f977ec8ad66d15b7b1f15db35
Robby Dore <rdore88@gmail.com>
2017-06-21 18:20:40 -0400

    Set up packages for use.

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const validator = require('express-validator');
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './public/views');
+app.set('view engine', 'mustache');
+
+app.use(bodyParser());
+app.express.static('public');
+app.use(validator());
